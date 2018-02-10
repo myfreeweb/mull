c456a557ebea8638255b9281e28801c7e57c02ad
AlexDenisov <1101.debian@gmail.com>
2018-02-10 16:33:33 +0100

    [Junk Detection] Add libclang as a dependency

--- include/JunkDetection/CXX/CXXJunkDetector.h
+++ include/JunkDetection/CXX/CXXJunkDetector.h
@@ -2,13 +2,23 @@
 
 #include "JunkDetection/JunkDetector.h"
 
+#include <map>
+#include <string>
+#include <clang-c/Index.h>
+
 namespace mull {
 
 class MutationPoint;
 
 class CXXJunkDetector : public JunkDetector {
 public:
+  CXXJunkDetector();
+  ~CXXJunkDetector();
+
   bool isJunk(MutationPoint *point) override;
+private:
+  CXIndex index;
+  std::map<std::string, CXTranslationUnit> units;
 };
 
 }
--- lib/JunkDetection/CXX/CXXJunkDetector.cpp
+++ lib/JunkDetection/CXX/CXXJunkDetector.cpp
@@ -3,6 +3,17 @@
 
 using namespace mull;
 
+CXXJunkDetector::CXXJunkDetector() : index(clang_createIndex(true, true)) {
+
+}
+
+CXXJunkDetector::~CXXJunkDetector() {
+  for (auto &pair : units) {
+    clang_disposeTranslationUnit(pair.second);
+  }
+  clang_disposeIndex(index);
+}
+
 bool CXXJunkDetector::isJunk(MutationPoint *point) {
   return false;
 }
