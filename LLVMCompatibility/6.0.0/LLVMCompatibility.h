2798e0d62dd94c250667fc004c480838caad0f29
AlexDenisov <1101.debian@gmail.com>
2018-06-06 21:06:42 +0200

    Add support for LLVM 5.0.x and 6.0.0

---
+++ LLVMCompatibility/6.0.0/LLVMCompatibility.h
@@ -0,0 +1,15 @@
+
+#include <llvm/ExecutionEngine/RuntimeDyld.h>
+#include <llvm/ExecutionEngine/SectionMemoryManager.h>
+#include <llvm/Bitcode/BitcodeReader.h>
+
+namespace llvm_compat {
+  using namespace llvm;
+
+  typedef JITSymbolResolver ORCResolver;
+  typedef JITSymbol ORCSymbolInfo;
+  typedef JITSymbol ORCJITSymbol;
+
+  uint64_t JITSymbolAddress(ORCJITSymbol &symbol);
+  JITSymbolFlags JITSymbolFlagsFromObjectSymbol(const object::BasicSymbolRef &symbol);
+}
