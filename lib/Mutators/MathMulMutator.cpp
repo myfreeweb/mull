a615dc80eb17cabb0a16cddd75fa02c9f45ef4f2
AlexDenisov <1101.debian@gmail.com>
2018-09-16 11:01:19 +0200

    Add LLVM-3.8 support

--- lib/Mutators/MathMulMutator.cpp
+++ lib/Mutators/MathMulMutator.cpp
@@ -59,10 +59,10 @@ llvm::Value *MathMulMutator::applyMutation(llvm::Module *module,
 
   /// NOTE: Create a new BinaryOperator with the same name as existing one
 
-  Instruction *replacement = BinaryOperator::Create(type,
-                                                    binaryOperator->getOperand(0),
-                                                    binaryOperator->getOperand(1),
-                                                    binaryOperator->getName());
+  BinaryOperator *replacement = BinaryOperator::Create(type,
+                                                       binaryOperator->getOperand(0),
+                                                       binaryOperator->getOperand(1),
+                                                       binaryOperator->getName());
   assert(replacement);
   if (binaryOperator->hasNoUnsignedWrap()) {
     replacement->setHasNoUnsignedWrap();
